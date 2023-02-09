ce9fef6b65d330f4773d285ffd48850f6676ab57
valfink <valentin.finke@gmail.com>
2023-02-09 15:49:32 +0100

    Project setup

---
+++ src/main/java/com/example/cgn231booklibraryrest/Cgn231BookLibraryRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.cgn231booklibraryrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cgn231BookLibraryRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cgn231BookLibraryRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cgn231booklibraryrest/Cgn231BookLibraryRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cgn231booklibraryrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cgn231BookLibraryRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
